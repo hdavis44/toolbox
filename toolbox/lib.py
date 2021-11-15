51859bcab75eeecee2929c414882a33e36de7b48
Henry Davis <henryo.davis@gmail.com>
2021-11-15 14:42:45 +0100

    Initial Push including a basic 'init_model' function

---
+++ toolbox/lib.py
@@ -0,0 +1,26 @@
+# library of interesting functions
+
+
+#import numpy as np
+#import pandas as pd
+#import matplotlib.pyplot as plt
+from tensorflow.keras import models, layers
+
+
+def init_model(input_dim=10):
+    """instantiate a basic binary classification neural network model
+       accepts an integer, input_dim, used to create first dense layer"""
+    model = models.Sequential()
+    model.add(layers.Dense(20, activation='relu', input_dim=input_dim))
+    model.add(layers.Dense(10, activation='relu'))
+    model.add(layers.Dense(1, activation='sigmoid'))
+
+    model.compile(loss='binary_crossentropy',
+                  optimizer='adam',
+                  metrics=['accuracy'])
+
+    return model
+
+if __name__ == '__main__':
+    model = init_model()
+    model.summary()
